d905307b07b69b6e742bd8d831aa0c668e3f7bf0
mattdrapkin <matt@drapkin.org>
2022-01-18 21:09:35 -0500

    Implemented UI with JS

--- rps.js
+++ rps.js
@@ -1,3 +1,15 @@
+let userScore = 0;
+let cpuScore = 0;
+let rounds = 0;
+const rock = document.querySelector(".rock-btn");
+const paper = document.querySelector(".paper-btn");
+const scissors = document.querySelector(".scissors-btn");
+const narrator = document.querySelector('.narrator');
+const scoreboard = document.getElementById('scoreboard');
+const userPtDisplay = document.getElementById('user-score');
+const CPUPtDisplay = document.getElementById('cpu-score');
+const rdDisplay = document.getElementById('rounds');
+
 function computerPlay() {
     let move = Math.random();
     if (move < 1/3) {
@@ -15,55 +27,79 @@ function capitalize(word) {
 }
 
 function playRound(playerSelection, computerSelection) {
+    rounds++;
     let player = capitalize(playerSelection);
     let tie = 'Tie!  You both played ' + player;
-    let win = 'You win! ' + player + ' beats ' + computerSelection;
-    let lose = 'You lose! ' + computerSelection + ' beats ' + player;
+    let win = 'You win! ' + player + ' beats the CPU\'s ' + computerSelection;
+    let lose = 'You lose! CPU selected ' + computerSelection + ', which beats ' + player;
 
     switch (player) {
         case ('Rock'):
             if (computerSelection === 'Rock') {
-                return tie;
+                narrator.innerHTML = tie;
             }
             if (computerSelection === 'Paper') {
-                return lose;
+                cpuScore++;
+                narrator.innerHTML = lose;
             }
             if (computerSelection === 'Scissors') {
-              return win;  
+                userScore++;
+                narrator.innerHTML = win;  
             }
             break;
         case ('Paper'):
             if (computerSelection === 'Rock') {
-                return win;
+                userScore++;
+                narrator.innerHTML = win;
             }
             if (computerSelection === 'Paper') {
-                return tie;
+                narrator.innerHTML = tie;
             }
             if (computerSelection === 'Scissors') {
-              return lose;  
+                cpuScore++;
+                narrator.innerHTML = lose;  
             }
             break;
         case ('Scissors'):
             if (computerSelection === 'Rock') {
-                return lose;
+                cpuScore++;
+                narrator.innerHTML = lose;
             }
             if (computerSelection === 'Paper') {
-                return win;
+                userScore++;
+                narrator.innerHTML = win;
             }
             if (computerSelection === 'Scissors') {
-                return tie;  
+                narrator.innerHTML = tie;
             }
             break;
         default:
             return 'Invalid move!';
     }
-}
+    userPtDisplay.innerHTML = "User: " + userScore;
+    CPUPtDisplay.innerHTML = "CPU: " + cpuScore;
+    rdDisplay.innerHTML = "Rounds completed: " + rounds;
 
-function game() {
-    let input = prompt("Rock, Paper, Scissors, SHOOT!  Your move:");
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(input, computerPlay()));
+    if(cpuScore >= 5) {
+        narrator.innerHTML = "Sorry! You have lost! Refresh to play again."
+        rock.parentNode.removeChild(rock);
+        paper.parentNode.removeChild(paper);
+        scissors.parentNode.removeChild(scissors);
+    }
+    if(userScore >= 5) {
+        narrator.innerHTML = "Congrats! You have won! Refresh to play again."
+        rock.parentNode.removeChild(rock);
+        paper.parentNode.removeChild(paper);
+        scissors.parentNode.removeChild(scissors);
     }
 }
 
-game();
+rock.addEventListener("click", () => {
+    playRound('rock', computerPlay())
+});
+paper.addEventListener("click", () => {
+    playRound('paper', computerPlay())
+});
+scissors.addEventListener("click", () => {
+    playRound('scissors', computerPlay())
+});
